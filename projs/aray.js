45dfed4267b8a674f005524c83285f3ba09675fc
imr4nn <xmartydecent@gmail.com>
2025-03-13 13:39:18 +0500

    ADD

--- projs/aray.js
+++ projs/aray.js
@@ -1,8 +1,18 @@
 let a = [1, 4, 7, 8, 2];
-console.log(a)
 
 let imran =
 {
     1: "HAHA"
 }
-console.log(a.length)
+a.push(48888)
+a.pop();
+a.pop();
+a.pop();
+a.pop();
+a.pop();
+a.pop();
+a.pop();
+a.pop();
+console.log(a)
+
+// console.log(a.length)
