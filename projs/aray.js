8d3474cd9570cd830cd52cea0a15334169658bc2
imr4nn <xmartydecent@gmail.com>
2025-03-13 13:30:16 +0500

    Day 41

---
+++ projs/aray.js
@@ -0,0 +1,8 @@
+let a = [1, 4, 7, 8, 2];
+console.log(a)
+
+let imran =
+{
+    1: "HAHA"
+}
+console.log(a.length)
---
+++ projs/s.js
@@ -0,0 +1,31 @@
+// alert("Helo kese hoo! ")
+// console.log("Welco")
+var a = 3
+// while (a != "Ali") {
+//     alert("You are not Ali")
+//      a = prompt("Enter Name");
+
+// }
+let b =
+{
+    "+": "Ali",
+    "-": "Imran",
+}
+// let c = "imran"
+c = b["+"]
+console.log(c)
+// function sum(a, b) {
+//     return c;
+// }
+// console.log(sum(2, 3))
+// console.log(sum(2, 3))
+// console.log(sum(2, 3))
+// console.log(sum(2, 3))
+// console.log(sum(2, 3))
+// console.log(sum(2, 3))
+// console.log(Math.random() < 0.1)
+console.log(`His name is ${c} & his friends name is ${b["-"]}`)
+
+for (let i = 0; i <= 4; i++) {
+    console.log(i)
+}
